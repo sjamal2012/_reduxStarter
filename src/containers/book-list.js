7949d0d5cf363e0ffed345ea387cf67f14cbff03
Sam Jamal <sammy@ultra.me>
2018-06-20 10:10:03 -0700

    finished Book-List app

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';  
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map( (book) => {
       return (
-        <li key={book.title} className='list-group-item'>{book.title}</li>
+        <li
+          onClick={ () => this.props.selectBook(book) }
+          key={book.title}
+          className='list-group-item'>
+          {book.title}
+        </li>
       );
     });
   }
@@ -17,3 +25,16 @@ export default class BookList extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
