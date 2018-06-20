da6cb4f18dddefe26a23adb90dfbdc01be2193d7
Sammy Jamal <sammyjamal12@gmail.com>
2018-06-20 07:01:52 -0700

    initial commit: small code

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div className='search-bar'>
+        <input
+          value={this.state.term}
+          onChange={ event => this.onInputchange(event.target.value)} />
+      </div>
+    );
+  }
+
+  onInputchange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
